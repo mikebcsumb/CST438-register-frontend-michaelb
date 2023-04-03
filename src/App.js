6188ea039216747bba3c1ec68577512e8fb685ad
Michael <mbradley@csumb.edu>
2023-04-03 15:56:48 -0700

    implemented feature add student.. still in progress

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import SchedList from './components/SchedList';
 import Semester from './components/Semester';
+//import Student from './components/Student';
+import StudentList from './components/StudentList';
+
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 function App() {
@@ -20,6 +23,7 @@ function App() {
        <Switch>
         <Route exact path='/' component={Semester} />
         <Route path='/schedule' component={SchedList} />
+		<Route path='/students' component={StudentList} />		
        </Switch>
       </BrowserRouter>
     </div>
